c279e41a3e70040950e831d222cf37ade685d6c0
Tnnobby <tnnobby@gmail.com>
2022-12-23 10:58:05 -0700

    fix: improved recipe typing

--- js/api/firebase.d.ts
+++ js/api/firebase.d.ts
@@ -1,3 +1,4 @@
+import { CameraCapturedPicture } from "expo-camera";
 import { Timestamp } from "firebase/firestore";
 
 type FirebaseTimestamp = {
@@ -16,19 +17,22 @@ export type NutritionFacts = {
   protein: number;
 }
 
+export type RecipeDetails = {
+  cooktime: HourMinuteTime;
+  description: string;
+  ingredients: string[];
+  instructions: string[];
+  nutrition_facts: NutritionFacts;
+  photo: string;
+  preptime: HourMinuteTime;
+  preheat: number;
+  title: string;
+}
+
 export interface Recipe {
   created_at: FirebaseTimestamp;
-  details: {
-    cooktime: HourMinuteTime;
-    description: string;
-    ingredients: string[];
-    instructions: string[];
-    nutrition_facts: NutritionFacts;
-    photo: string;
-    preptime: HourMinuteTime;
-    preheat: number;
-    title: string;
-  };
+  details: RecipeDetails;
   id: string;
   searchable_title: string;
+  owner_id: string;
 }
