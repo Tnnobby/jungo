92a1d39600c5af25ed5be00738fc35fb0555108b
Tnnobby <tnnobby@gmail.com>
2022-12-23 11:06:50 -0700

    fix: api typescript conversion and adjustments

--- js/api/useAuth.tsx
+++ js/api/useAuth.tsx
@@ -14,7 +14,9 @@ import {
 } from "firebase/firestore";
 import { useEffect, useRef, useContext } from "react";
 import firebaseConfig from "../../firebase.config";
-import { FirebaseContext } from "../context-providers/FirebaseProvider";
+import { FirebaseContext, UserDoc } from "../context-providers/FirebaseProvider";
+
+type LoginMethods = 'email'
 
 export default function useAuth() {
   const { auth, db, actions } = useContext(FirebaseContext);
@@ -25,17 +27,22 @@ export default function useAuth() {
   const newUser = async ({ method, email, password }) => {
     switch (method) {
       case "email":
+        actions.dispatchUser({ type: "SET_NEW_USER" });
         return createUserWithEmailAndPassword(auth, email, password);
       default:
         return Promise.reject("Invalid Method");
     }
   };
 
-  /**
-   * @param {{method:'email' | null, email?: string, password?: string}} param0
-   * @returns
-   */
-  const loginUser = async ({ method, email, password }) => {
+  const loginUser = async ({
+    method,
+    email,
+    password,
+  }: {
+    method: LoginMethods;
+    email?: string;
+    password?: string;
+  }) => {
     return new Promise((resolve, reject) => {
       switch (method) {
         case "email":
@@ -55,21 +62,23 @@ export default function useAuth() {
 
   const createUserDoc = async ({ uid, data }) => {
     return new Promise((resolve, reject) => {
-      const userDoc = {
+      const userDoc: UserDoc = {
         ...data,
         created_at: Timestamp.now(),
-        birthday: Timestamp.fromDate(data.birthday),
+        private: {
+          birthday: Timestamp.fromDate(data.birthday),
+        }
       };
-      // FIXME : Currently dispatching userdoc right after it is created, maybe not a good place to handle this, think of a better place to put this.
       setDoc(doc(db, "users", uid), userDoc)
         .then(() => {
           actions.dispatchUser({
-            type: "FETCH_USER_DOC_SUCCESS",
+            type: "SET_NEW_USER_DOC",
             doc: userDoc,
           });
           resolve(userDoc);
         })
         .catch((reason) => reject(reason));
+        
     });
   };
 
---
+++ js/api/useFirebaseRecipes.ts
@@ -0,0 +1,115 @@
+import {
+  addDoc,
+  collection,
+  endAt,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  startAt,
+  Timestamp,
+} from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+import { v1 } from "uuid";
+import { useState } from "react";
+import { Recipe, RecipeDetails } from "./firebase";
+import useFirebase from "./useFirebase";
+
+// TODO : add types here
+export default function useFirebaseRecipes() {
+  const [recipes, setRecipes] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const { storage, db, user } = useFirebase();
+
+  const addRecipe = async (data: RecipeDetails) => {
+    const id = v1();
+    let recipePhoto: string;
+    if (data.photo) {
+      recipePhoto = (
+        await addPhoto(data.photo, id)
+      ).ref.fullPath;
+    }
+    const recipeData: Recipe = {
+      details: {
+        ...data,
+        photo: recipePhoto
+      },
+      owner_id: user.uid,
+      created_at: Timestamp.now(),
+      id: id,
+      searchable_title: data.title.toLowerCase()
+    }
+    return addDoc(collection(db, "recipes"), recipeData)
+      .then(() => recipeData)
+  };
+
+  const addPhoto = async (uri: string, id: string) => {
+    const photo = await (await fetch(uri.replace("file://", "file:"))).blob();
+    return uploadBytes(ref(storage, `recipe_photos/${id}.jpg`), photo);
+  };
+
+  const getRecipes = async (count = 10) => {
+    console.log("requesting recipes");
+    const start =
+      recipes.length > 0 ? recipes[recipes.length - 1].created_at : null;
+    const q = start
+      ? query(
+          collection(db, "recipes"),
+          orderBy("created_at"),
+          limit(count),
+          startAfter(start)
+        )
+      : query(collection(db, "recipes"), orderBy("created_at"), limit(count));
+    return new Promise((resolve, reject) => {
+      getDocs(q)
+        .then((response) => {
+          const responseData = response.docs.map((d) => d.data());
+          setRecipes((r) => [...r, ...responseData]);
+          resolve(true);
+        })
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        });
+    });
+  };
+
+  const searchRecipes = (searchTerm: string) => {
+    console.log("hook: ", searchTerm);
+    const q = query(
+      collection(db, "recipes"),
+      orderBy("searchable_title"),
+      startAt(searchTerm),
+      endAt(searchTerm + "~")
+    );
+    return new Promise((resolve, reject) => {
+      getDocs(q)
+        .then((response) => {
+          if (response.empty) {
+            setRecipes([]);
+            resolve(false);
+            return;
+          }
+          const responseData = response.docs.map((d) => d.data());
+          setSearchResults(responseData);
+          resolve(true);
+        })
+        .catch((error) => {
+          console.error(error);
+          reject(error);
+        });
+    });
+  };
+
+  const clearRecipes = () => setRecipes([]);
+
+  return {
+    addRecipe,
+    getRecipes,
+    searchRecipes,
+    clearRecipes,
+    searchResults,
+    recipes,
+  };
+}
